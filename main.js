dcb7cdfe4aeb81f41d248c38b130317b07c05bcf
Aaron Kim <ajkim19@gmail.com>
2025-01-03 18:35:45 +0000

    Updated remade table
    Rows of the table were removed from index.html and repopulated using Java

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const eventSchedulerTime = document.getElementById("event-scheduler-time");
+if (!eventSchedulerTime) throw new Error("eventSchedulerTime does not exist");
+const scheduleListTableBody = document.querySelector("schedule-list-table > tbody")
+
 
 for (let i = 0; i < 24; i++) {
 	const hr = i.toString().padStart(2, "0");
@@ -10,4 +13,13 @@ for (let i = 0; i < 24; i++) {
 	onHalfHour.value = `${hr}30`;
 	onHalfHour.text = `${hr}30`;
 	eventSchedulerTime.appendChild(onHalfHour);
+}
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement("tr");
+  for (let j = 0; j < 3); j++) {
+    const cell = document.createElement("td");
+    row.appendChild(cell);
+  }
+  scheduleListTableBody.appendChild(row);
 }
